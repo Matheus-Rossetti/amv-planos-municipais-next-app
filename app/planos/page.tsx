eccbeeed7e5dd798282d1e4179269f185a7bf7bb
Matheus Rossetti <matheuzdreher@gmail.com>
2025-05-21 12:27:53 -0300

    Refactor Planos component: improve layout and styling for better presentation

--- app/planos/page.tsx
+++ app/planos/page.tsx
@@ -1,28 +1,72 @@
 import React from 'react'
 
-export default function Planos(){
+export default function Planos() {
 
-const plans = [{nome: "nome", data: "2025/2026",  descrição: "descrição", imagem: "https://picsum.photos/1920/1080"}, {nome: "nome", descrição: "descrição", imagem: "https://picsum.photos/1920/1080"}, {nome: "nome", descrição: "descrição", imagem: "https://picsum.photos/1920/1080"}, {nome: "nome", descrição: "descrição", imagem: "https://picsum.photos/1920/1080"},]
+    const plans = [
+        {
+            nome: "nome",
+            data: "2025/2026",
+            descrição: "descrição",
+            imagem: "https://picsum.photos/1920/1080"
+        },
+        {
+            nome: "nome",
+            data: "2025/2026",
+            descrição: "descrição",
+            imagem: "https://picsum.photos/1920/1080"
+        },
+        {
+            nome: "nome",
+            data: "2025/2026",
+            descrição: "descrição",
+            imagem: "https://picsum.photos/1920/1080"
+        },
+        {
+            nome: "nome",
+            data: "2025/2026",
+            descrição: "descrição",
+            imagem: "https://picsum.photos/1920/1080"
+        },
+        {
+            nome: "nome",
+            data: "2025/2026",
+            descrição: "descrição",
+            imagem: "https://picsum.photos/1920/1080"
+        },
+        {
+            nome: "nome",
+            data: "2025/2026",
+            descrição: "descrição",
+            imagem: "https://picsum.photos/1920/1080"
+        },
+    ]
 
 
     return (
         <div style={{
             display: "flex",
-            flexDirection: "row"
+            flexDirection: "row",
+            gap: "1em",
+            justifyContent: "center",
+            margin: "10em",
+            flexWrap: "wrap",
         }}>
             {plans.map((plan, index) => {
-                return(
+                return (
                     <div key={index}
                          style={{
-                             height: "400px",
-                             width: "200px",
+                             height: "30em",
+                             width: "20em",
                              backgroundColor: "green",
                              display: "flex",
                              flexDirection: "column",
                          }}>
                         <div>{plan.nome}</div>
                         <div>{plan.data}</div>
-                        <div><img src={plan.imagem}/></div>
+                        <img src={plan.imagem} style={{
+                            objectFit: "cover",
+                            height: "30%", width: "100%", marginRight: "5%", marginLeft: "5%"
+                        }}/>
                         <div>{plan.descrição}</div>
                     </div>
                 )
